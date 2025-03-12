60246d7f24ae3fde68318dfa2d185ac7cf542598
Nikhil-Verma-01 <nik.h0ill907@gmail.com>
2025-03-12 15:40:49 +0530

    LeftSidebar

--- constants/index.ts
+++ constants/index.ts
@@ -1,12 +1,17 @@
-export const sidebarLinks = [
-    {
-        route:'/profile',
-        label: 'profile',
-        imgURL: '/icons/mircophone.svg'
-    },
+export const sidebarLinks = [ 
     {
         route:'/home',
         label: 'Home',
         imgURL: '/icons/home.svg'
+    },
+    {
+        route: '/discover',
+        label: 'Discover',
+        imgURL: '/icons/discover.svg'
+    },
+    {
+        route:'/profile',
+        label: 'profile',
+        imgURL: '/icons/microphone.svg'
     },
 ];
